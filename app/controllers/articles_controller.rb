ecf9064d0facca03924d31be45da76cbc95fa0dc
BenDaG <benbrewerbowman@gmail.com>
2017-03-11 17:57:09 -0500

    delete added, edit/update added, form partial created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,48 @@
 class ArticlesController < ApplicationController
 
+  include ArticlesHelper
+
+  # list of articles
   def index
     @articles = Article.all
   end
 
+  # display article
   def show
     @article = Article.find(params[:id])
   end
-  
+
+  # new article in DB
+  def new
+    @article = Article.new
+  end
+
+  # create a new article
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  # edit an article
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # update an edit
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
+  # delete an article
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path()
+  end
+
 end
