45181eafcde1b9d20836bdcd1bc0e3a50cf158d4
BenDaG <benbrewerbowman@gmail.com>
2017-03-12 23:18:28 -0400

    added tag index, added tag delete option

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,23 @@
 class TagsController < ApplicationController
 
+  # list of tags
+  def index
+    @tag = Tag.all
+  end
+
+  # display tag
   def show
     @tag = Tag.find(params[:id])
+  end
+
+  # delete a tag
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    flash.notice = "Tag '#{@tag.name}' was successfully deleted"
+
+    redirect_to action: 'index'
   end
 
 end
